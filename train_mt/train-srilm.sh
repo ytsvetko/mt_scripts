42700f94cebcae9b20c7b2661bd0eede26be2065
ytsvetko <yulia.tsvetkov@gmail.com>
2014-02-09 12:29:17 -0500

    fixed train-srilm.sh

--- train_mt/train-srilm.sh
+++ train_mt/train-srilm.sh
@@ -6,28 +6,25 @@ SRILM=${TOOLS_DIR}/srilm/bin/i686-m64
 TRAIN_LM=$SRILM/ngram-count 
 TRAIN_LM_PARAM=" -gt1min 1 -gt2min 1 -gt3min 1 -gt4min 1 -kndiscount1 -kndiscount2 -kndiscount3 -kndiscount4"
 COMPUTE_BEST_MIX=$SRILM/compute-best-mix
-INTERPOLATE=$SRILM/ngram
+NGRAM=$SRILM/ngram
 
 # LM Settings
 LM_ORDER=4
-DATA_DIR=$PWD/../data/cdec-spanish-demo
-LM_DIR=${DATA_DIR}/lm
+DATA_DIR=$PWD/../data/data.tokenized
+LM_DIR=$PWD/../lm
 LM=${LM_DIR}/`basename ${DATA_DIR}`.${LM_ORDER}grams.arpa
 PPL_DIR=${LM_DIR}/ppl
 LOG_DIR=${LM_DIR}/log
 
 # Dev corpus for tuning LMs interpolation weights
-DEV=${DATA_DIR}/dev/2010.es-en.en
+DEV=${DATA_DIR}/dev.es-en.en
 
 # Textfiles and LMs for interpolation
-text[${#text[*]}]=${DATA_DIR}/training/news-commentary-v7.es-en.en
+text[${#text[*]}]=${DATA_DIR}/train.es-en.en
 type[${#type[*]}]="text"                                                            
 
-text[${#text[*]}]=${DATA_DIR}/mono/europarl-v7.en
+text[${#text[*]}]=${DATA_DIR}/test.es-en.en
 type[${#type[*]}]="text"                                                         
-
-text[${#text[*]}]="none"
-type[${#type[*]}]="ngram"
 
 ############################################################
 
@@ -35,13 +32,8 @@ type[${#type[*]}]="ngram"
 mkdir -p ${LM_DIR} 
 mkdir -p ${PPL_DIR}
 
-
-for ((i=0;i<${#text};i++)); do
+for ((i=0;i<${#text[*]};i++)); do
    t=${text[${i}]}
-   if [ "$t" == "none" ] ; then
-     break
-   fi
-
    fname=${t//*\/}
    pplname[$i]="${PPL_DIR}/${fname}.ppl"
    lmname[$i]="${LM_DIR}/${fname}.txt"
@@ -56,37 +48,29 @@ for ((i=0;i<${#text};i++)); do
   elif [ "${type[${i}]}" == "ngram" ]; then # Arpa file
     (gzip -dfc $t > ${lmname[${i}]})
   elif [ "${type[${i}]}" == "bigngram" ]; then # Large arpa file to prune 
-    $INTERPOLATE -order $LM_ORDER -lm $t -prune 1e-10 -write-lm ${lmname[${i}]} 
+    $NGRAM -order $LM_ORDER -lm $t -prune 1e-10 -write-lm ${lmname[${i}]} 
   else
     echo "\"${type[${i}]}\" is a totaly unknown type..." 
     exit 0
   fi
   
   echo "ppltest: ${pplname[${i}]}" 
-  $INTERPOLATE -order $LM_ORDER -debug 2 -lm "${lmname[${i}]}" -ppl $DEV >& "${pplname[${i}]}"
+  $NGRAM -order $LM_ORDER -debug 2 -lm "${lmname[${i}]}" -ppl $DEV >& "${pplname[${i}]}"
 done
 
 mixParam=""
 mixOutput="${PPL_DIR}/best-mix.txt"
-for ((i=0;i<${#text};i++)); do
-   t=${text[${i}]}
-   if [ "$t" == "none" ] ; then
-     break
-   fi
-   mixParam="${mixParam}${pplname[${i}]} "
+for ((i=0;i<${#text[*]};i++)); do
+   mixParam="${mixParam} ${pplname[${i}]}"
 done
 echo "finding best mixing parameters"
 
 $COMPUTE_BEST_MIX $mixParam >& $mixOutput
+BEST_MIX=`cat $mixOutput | grep -Eo "best lambda \((.*)\)" | grep -Eo "\((.*)\)" | grep -Eo "[^()]*"`
 
 intParam=""
-for ((i=0;i<${#text};i++)); do
-   t=${text[${i}]}
-   if [ "$t" == "none" ] ; then
-     break
-   fi
-   j=$(( $i + 6 ))
-   lambda[$i]=`tail -n 1 $mixOutput| cut -f$j  -d" "| perl -pe 's/[\)\(\,]//ig;if(m/(\d)\.(\d+)e\-(\d+)/ig){$i=($3-1);while($i--){$pre.="0"}$_="0\.$pre$1$2";}'`
+for ((i=0;i<${#text[*]};i++)); do
+   lambda[$i]=`echo ${BEST_MIX} | cut -f$((i + 1))  -d" "`
    echo -e "${lambda[${i}]} \t for ${lmname[${i}]}" 
    if [ "$i" == "0" ]; then
      intParam="$intParam -lm ${lmname[${i}]} -lambda ${lambda[${i}]}"
@@ -98,8 +82,8 @@ for ((i=0;i<${#text};i++)); do
 done
 
 echo "Interpolating LMS"
-$INTERPOLATE -order $LM_ORDER -debug 2 $intParam -write-lm $LM 
-$INTERPOLATE -order $LM_ORDER -lm $LM -ppl $DEV
+$NGRAM -order $LM_ORDER -debug 2 $intParam -write-lm $LM 
+$NGRAM -order $LM_ORDER -lm $LM -ppl $DEV
 
 # ppl gives us the geometric average of 1/probability of each token, i.e., perplexity. 
 # The exact expression is:
